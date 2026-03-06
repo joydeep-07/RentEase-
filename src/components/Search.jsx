e1404c2fb53bcac0cc05a64391144de6975a54d2
Joydeep Paul <joydeeprnp8821@gmail.com>
2026-03-06 22:26:07 +0530

    Updated UI

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,8 +5,8 @@ export default function Search() {
     <div className="flex items-center border pr-3 gap-2 bg-[var(--bg-seconadary)] border-[var(--border-light)]/50 h-[46px] rounded-full overflow-hidden w-2xl ">
       <input
         type="text"
-        placeholder="Search for furnitures"
-        className="w-full h-full pl-5 outline-none text-[var(--text-secondary)] placeholder-[var(--text-muted)] text-sm"
+        placeholder="Search for furnitures ..."
+        className="w-full h-full pl-5 outline-none text-[var(--text-secondary)] placeholder-[var(--text-muted)]/50 text-sm"
       />
       <button type="button">
         <SearchIcon
