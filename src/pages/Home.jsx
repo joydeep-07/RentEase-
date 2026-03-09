75a72ff52d88f9548374288c01d7096f2524696e
Joydeep Paul <joydeeprnp8821@gmail.com>
2026-03-09 12:00:03 +0530

    Home Updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
-import Hero from '../components/Hero'
-import Furniture from '../components/Furniture'
 import { data } from "../utils/data";
-import ItemCards from "../components/ItemCards";
+import ItemCards from '../components/ItemCards';
 const Home = () => {
-  const furniture = data.filter((item) => item.category === "Furniture");
+    const furniture = data.filter((item) => item.category === "Furniture");
   return (
     <div>
-      {/* <Hero/> */}
-      {/* <Furniture/> */}
       <ItemCards items={furniture} limit={8} />
     </div>
   );
